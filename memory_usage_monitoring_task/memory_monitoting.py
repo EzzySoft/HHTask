0cf23364f42feffb67b9efd9b012c5c95533a548
Ezzy <d.tretiakov@innopolis.university>
2023-10-21 15:25:08 +0300

    First task implemented

---
+++ memory_usage_monitoring_task/memory_monitoting.py
@@ -0,0 +1,38 @@
+import time
+
+import psutil
+import requests
+
+URL_API = "https://testsevice.com/api/alarm"
+
+MEMORY_THRESHOLD_PERCENT = 85
+MESSAGES_COOLDOWN_SECONDS = 10
+
+
+def check_memory_usage():
+    memory_percent = psutil.virtual_memory().percent
+    if memory_percent > MEMORY_THRESHOLD_PERCENT:
+        send_alarm()
+        print(f"Waiting {MESSAGES_COOLDOWN_SECONDS} seconds...")
+        time.sleep(MESSAGES_COOLDOWN_SECONDS)
+
+
+def send_alarm():
+    data = {
+        "message": "Memory consumption has exceeded the threshold!",
+        "memory_percent": psutil.virtual_memory().percent
+    }
+
+    try:
+        response = requests.post(URL_API, json=data, timeout=10)
+        if response.status_code == 200:
+            print("Alarm message sent")
+        else:
+            print(f"Error while sending message. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"Unexpected error while sending message: {str(e)}")
+
+
+if __name__ == "__main__":
+    while True:
+        check_memory_usage()
